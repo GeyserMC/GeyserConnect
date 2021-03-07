c73714c9b4e32a4366e470ee7ef3a928f5b297e7
rtm516 <ryantmilner@hotmail.co.uk>
2021-03-07 18:27:52 +0000

    Revert "Fix issues with hostnames on PS4"
    This reverts commit d180ab37951a1982d4ad32edfb3937b436e1ee36.

--- src/main/java/org/geysermc/connect/utils/Player.java
+++ src/main/java/org/geysermc/connect/utils/Player.java
@@ -43,7 +43,6 @@ import org.geysermc.connect.ui.FormID;
 import org.geysermc.connect.ui.UIHandler;
 import org.geysermc.connector.network.session.auth.BedrockClientData;
 
-import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -213,13 +212,9 @@ public class Player {
             port = currentServer.getPort();
         }
 
-        // Create an InetSocketAddress to reduce issues with hostnames for PS4
-        // Thanks Extollite
-        InetSocketAddress socketAddress = new InetSocketAddress(address, port);
-
         TransferPacket transferPacket = new TransferPacket();
-        transferPacket.setAddress(socketAddress.getAddress().getHostAddress());
-        transferPacket.setPort(socketAddress.getPort());
+        transferPacket.setAddress(address);
+        transferPacket.setPort(port);
         session.sendPacket(transferPacket);
     }
 
