445f9b63df3979a4cec67264685e70e3ab40fa19
xvdg06 <61436375+xvdg06@users.noreply.github.com>
2021-04-14 03:52:23 +0100

    Change placeholder for server address

--- src/main/java/org/geysermc/connect/ui/UIHandler.java
+++ src/main/java/org/geysermc/connect/ui/UIHandler.java
@@ -111,7 +111,7 @@ public class UIHandler {
      */
     public static FormWindow getDirectConnect() {
         return new CustomFormBuilder("Direct Connect")
-                .addComponent(new InputComponent("IP", "play.cubecraft.net", ""))
+                .addComponent(new InputComponent("IP", "Server address", ""))
                 .addComponent(new InputComponent("Port", "25565", "25565"))
                 .addComponent(new ToggleComponent("Online mode", true))
                 .addComponent(new ToggleComponent("Bedrock server", false))
